133eeae16bfbd33cccb8baf298c8a22f90537f39
Arturas Aleksandraus <Arturas.Aleksandraus@contextvision.se>
2024-05-14 13:25:19 +0200

    Optimize hyperparameters for OOD Detectors

--- src/optimize.py
+++ src/optimize.py
@@ -12,6 +12,7 @@ import torch
 import ood_detectors.ops_utils as ops_utils
 import multiprocessing as mp
 import pickle
+import random
 # data = {
 #         'features': features_vectors,
 #         'encoder': encoder_name,
@@ -25,7 +26,7 @@ def select_trial(trial,method):
     if method == 'Residual':
         conf['dims'] = trial.suggest_float('Residual.dims', 0, 1)
     else:
-        conf['n_epochs'] = trial.suggest_int('n_epochs', 100, 10000, seps=100)
+        conf['n_epochs'] = trial.suggest_int('n_epochs', 100, 1000, step=100)
         conf['bottleneck_channels'] = trial.suggest_int('bottleneck_channels', 256, 2048, step = 256)
         conf['num_res_blocks'] = trial.suggest_int('num_res_blocks', 3, 15)
         conf['time_embed_dim'] = trial.suggest_int('time_embed_dim', 256, 1024, step = 256)
@@ -35,9 +36,10 @@ def select_trial(trial,method):
         conf['beta2'] = trial.suggest_float('beta2', 0.9, 0.999)
         conf['eps'] = trial.suggest_float('eps', 1e-12, 1e-6, log=True)
         conf['weight_decay'] = trial.suggest_float('weight_decay', 0.0, 1e-3)
-        conf['batch_size'] = trial.suggest_int('batch_size', 32, 256)
-
-        conf['continuous'] = trial.suggest_categorical('continuous', [True, False])
+        if method != 'subVPSDE':
+            conf['continuous'] = trial.suggest_categorical('continuous', [True, False])
+        else:
+            conf['continuous'] = False
         conf['reduce_mean'] = trial.suggest_categorical('reduce_mean', [True, False])
         conf['likelihood_weighting'] = trial.suggest_categorical('likelihood_weighting', [True, False])
         if method == 'VESDE':
@@ -55,6 +57,7 @@ def select_trial(trial,method):
 
 def run(conf, data, encoder, dataset, method, device):
     train_data_path = data[encoder][dataset]["id"]["train"]
+    batch_size = conf.get('batch_size', 1024)
     with open(train_data_path, 'rb') as f:
         train_blob = pickle.load(f)
 
@@ -76,7 +79,6 @@ def run(conf, data, encoder, dataset, method, device):
         beta2 = conf['beta2']
         eps = conf['eps']
         weight_decay = conf['weight_decay']
-        batch_size = conf['batch_size']
         continuous = conf['continuous']
         reduce_mean = conf['reduce_mean']
         likelihood_weighting = conf['likelihood_weighting']
@@ -128,6 +130,7 @@ def run(conf, data, encoder, dataset, method, device):
             n_epochs=n_epochs,
             batch_size=batch_size,
             update_fn=update_fn,
+            verbose=False,
         )
 
     score_id = ood_model.predict(train_blob['features'], batch_size, verbose=False)
@@ -138,41 +141,50 @@ def run(conf, data, encoder, dataset, method, device):
     results = {}
     id_auc = eval_utils.auc(-score_ref, -score_id)
     results['id'] = id_auc
-    for name, data in data[encoder][dataset].items():
+    for name, datasets in data[encoder][dataset].items():
         if name == 'id':
             continue
-        with open(data, 'rb') as f:
-            ood_data = pickle.load(f)
-        score_ood = ood_model.predict(ood_data['features'], batch_size, verbose=False)
-        ood_auc = eval_utils.auc(-score_ref, -score_ood)
-        results[name] = ood_auc
+        if name not in results:
+            results[name] = {}
+        for type_name, data in datasets.items():
+            with open(data, 'rb') as f:
+                ood_data = pickle.load(f)
+            score_ood = ood_model.predict(ood_data['features'], batch_size, verbose=False)
+            ood_auc = eval_utils.auc(-score_ref, -score_ood)
+            results[name][type_name] = ood_auc
     return results
 
-def objective(trial, data, encoder, dataset, method, device):
+def objective(trial, data, encoders, datasets, method, device):
     conf = select_trial(trial, method)
-    results = run(conf, data, encoder, dataset, method, device)
-    id = results['id']
-    farood = sum(results['farood']) / len(results['farood'])
-    nearood = sum(results['nearood']) / len(results['nearood'])
-    
+    ids = []
+    faroods = []
+    nearoods = []
+    for encoder in encoders:
+        for dataset in datasets:
+            results = run(conf, data, encoder, dataset, method, device)
+            id = results['id']
+            farood = sum(results['farood'].values()) / len(results['farood'])
+            nearood = sum(results['nearood'].values()) / len(results['nearood'])
+            ids.append(id)
+            faroods.append(farood)
+            nearoods.append(nearood)
+    id = sum(ids) / len(ids)
+    farood = sum(faroods) / len(faroods)
+    nearood = sum(nearoods) / len(nearoods)
     return abs(id-0.5), farood, nearood
 
-def ask_tell_optuna(objective_func, data, encoder, dataset, method, device):
-    study_name = f'{encoder}_{dataset}_{method}'
+def ask_tell_optuna(objective_func, data, encoders, datasets, method, device):
+    study_name = f'{method}'
     db = f'sqlite:///optuna.db'
     study = optuna.create_study(directions=['minimize', 'maximize', 'maximize'], study_name=study_name, storage=db, load_if_exists=True)
     trial = study.ask()
-    try:
-        res = objective_func(trial, data, encoder, dataset, method, device)
-    except Exception as e:
-        study.tell(trial, None)
-        print(e)
-        return
+    res = objective_func(trial, data, encoders, datasets, method, device)
     study.tell(trial, res)
         
 
 def main():
-    features = pathlib.Path("/mnt/data/arty/data/features_open_ood")
+    features = pathlib.Path(r"H:\arty\data\features_open_ood")
+    # features = pathlib.Path("/mnt/data/arty/data/features_open_ood")
     features_data = {}
     all_pkl = list(features.rglob("*.pkl"))
     for path in all_pkl:
@@ -185,15 +197,20 @@ def main():
         else:
             with open(path, "rb") as f:
                 tmp[parts[-2]] = path
-    encoders = ['repvgg', 'resnet50d', 'swin', 'deit', 'dino', 'dinov2', 'vit', 'clip', 'resnet18_32x32_cifar10_open_ood', 'resnet18_32x32_cifar100_open_ood', 'resnet18_224x224_imagenet200_open_ood', 'resnet50_224x224_imagenet_open_ood']
-    datasets = ['imagenet', 'imagenet200', 'cifar10', 'cifar100', 'covid', 'mnist']
-    encoder = 'repvgg'
-    dataset = 'covid'
-    method = 'Residual'
-    trials = 10
-    jobs = [(objective, features_data, encoder, dataset, method)]*trials
+    encoders = ['repvgg', 'resnet50d', 'swin', 'deit', 'dino', 'dinov2', 'vit', 'clip']
+    #['resnet18_32x32_cifar10_open_ood', 'resnet18_32x32_cifar100_open_ood', 'resnet18_224x224_imagenet200_open_ood', 'resnet50_224x224_imagenet_open_ood']
+    # datasets = ['imagenet', 'imagenet200', 'cifar10', 'cifar100', 'covid', 'mnist']
+    datasets = ['imagenet200', 'cifar10', 'cifar100']
+    methods = ['VESDE', 'VPSDE', 'subVPSDE', 'Residual']
+    jobs = []
+    for m in methods:
+        jobs.append((objective, features_data, encoders, datasets, m))
+       
+    trials = 100
     gpu_nodes = [0]
-    ops_utils.parallelize(ask_tell_optuna, jobs, gpu_nodes, verbose=True, timeout=60*60*24)
+    random.shuffle(jobs)
+    print(f'Running {len(jobs)} jobs...')
+    ops_utils.parallelize(ask_tell_optuna, jobs*trials, gpu_nodes, verbose=True, timeout=60*60*24)
 
 if __name__ == '__main__':
     mp.freeze_support()
