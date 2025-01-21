6e23a4c6b3dfb95caa8997bdc8fdbca10abdc901
Arturas Aleksandraus <Arturas.Aleksandraus@contextvision.se>
2025-01-21 10:31:49 +0100

    feat: Add GPU utilization metrics to get_nvml_info function

--- src/test.py
+++ src/test.py
@@ -1,4 +1,4 @@
-from pynvml import nvmlInit, nvmlDeviceGetCount, nvmlDeviceGetHandleByIndex, nvmlDeviceGetName, nvmlDeviceGetMemoryInfo
+from pynvml import nvmlInit, nvmlDeviceGetCount, nvmlDeviceGetHandleByIndex, nvmlDeviceGetName, nvmlDeviceGetMemoryInfo, nvmlDeviceGetUtilizationRates
 
 def get_nvml_info():
     try:
@@ -11,15 +11,15 @@ def get_nvml_info():
             memory = nvmlDeviceGetMemoryInfo(handle)
             print(f"GPU {i}: {name}")
             # in
-            print(f"  Total Memory: {memory.total / 1e-9:.2f} GB")
-            print(f"  Free Memory: {memory.free / 1e-9:.2f} GB")
-            print(f"  Used Memory: {memory.used / 1e-9:.2f} GB")
+            print(f"  Total Memory: {memory.total * 1e-9:.2f} GB")
+            print(f"  Free Memory: {memory.free * 1e-9:.2f} GB")
+            print(f"  Used Memory: {memory.used * 1e-9:.2f} GB")
 
 
             # utilization 
             print(f"  GPU Utilization: {nvmlDeviceGetUtilizationRates(handle).gpu} %")
             print(f"  Memory Utilization: {nvmlDeviceGetUtilizationRates(handle).memory} %") 
-            
+
     except Exception as e:
         print("Error accessing NVML:", e)
 
