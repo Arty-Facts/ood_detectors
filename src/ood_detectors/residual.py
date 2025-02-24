83dd5fed3c06b0c0b086a81e40ec66eeae841463
Arturas Aleksandraus <arturas@aleksandraus.se>
2025-02-24 15:31:48 +0100

    fix: Update to method in ResidualX class to ensure all detectors are moved to the specified device

--- src/ood_detectors/residual.py
+++ src/ood_detectors/residual.py
@@ -202,9 +202,11 @@ class ResidualX(torch.nn.Module):
         self.full_dims = full_dims
         self.mods = torch.nn.ModuleList(self.ood_detectors)
 
-    def to(self, device):
-        self.device = device
-        return self
+        def to(self, device):
+            self.device = device
+            for ood_detector in self.ood_detectors:
+                ood_detector.to(device)
+            return self
     
     def load_state_dict(self, state_dict):
         for ood_detector, state_dict in zip(self.ood_detectors, state_dict):
